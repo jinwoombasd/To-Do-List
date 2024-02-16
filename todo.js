df8e97cb05a89df488942c8cf919e9eb8e19c815
mbasd <115119366+jinwoombasd@users.noreply.github.com>
2024-02-16 16:54:37 +0000

    to do list

--- todo.js
+++ todo.js
@@ -2,11 +2,21 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function showToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
+    showToDo(newToDo);
 }
+
 
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
