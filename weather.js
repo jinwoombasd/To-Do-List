cab683054e3f76f2730a3fa13924098fc0f9d78d
mbasd <115119366+jinwoombasd@users.noreply.github.com>
2024-02-27 12:30:13 -0600

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,8 +1,16 @@
+const API_KEY = "4c32e4c9a496838e9a2cefa0bf346d23"
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const long = position.coords.longitude;
-    console.log("You live in", lat, long);
-}
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    })
+} 
 
 function onGeoEr(){
     alert("Error");
